11a74357010c19643567524cbda91dcd93c10ef5
Duc Le <hieuduc194@gmail.com>
2018-10-11 20:17:00 +0300

    add toggle menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MoneyFlow';
+
+  public ngOnInit()
+  {
+    $(document).ready(function() {
+      $('button.openbtn').on('click', function() {
+        $('#mySidebar').css('width', '250px');
+        $('#main').css('margin-left', '250px');
+      });
+    });
+
+    $(document).ready(function() {
+      $('a.closebtn').on('click', function() {
+        $('#mySidebar').css('width', '0px');
+        $('#main').css('margin-left', '');
+        return false;
+      });
+    });
+  }
 }
